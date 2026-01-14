1fbef70d0c9a924b9e4e284f8d03de485820946f
samuelpeids2024 <158535896+samuelpeids2024@users.noreply.github.com>
2026-01-14 17:29:23 +0800

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -38,7 +38,8 @@ messaging.onBackgroundMessage(function(payload) {
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: payload.notification.icon // 或指定一個圖示
+    icon: payload.notification.icon, // 或指定一個圖示
+    sound: "./notification.mp3"
   };
 
   self.registration.showNotification(notificationTitle,
